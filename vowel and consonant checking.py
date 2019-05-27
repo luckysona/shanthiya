d64273e2d8be12c91b9905d6f0ed2d85eba5324d
luckysona <50876072+luckysona@users.noreply.github.com>
2019-05-27 08:50:52 -0700

    Create vowel and consonant checking.py

---
+++ vowel and consonant checking.py
@@ -0,0 +1,7 @@
+ch=input()
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or         ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+	      print("Vowel")
+elif(ch=='b' or ch=='B' or ch=='c' or ch=='C' or ch=='d' or ch=='D' or ch=='j' or ch=='J' or ch=='f' or ch=='F' or ch=='g' or ch=='G' or ch=='h' or ch=='H' or ch=='k' or ch=='K' or ch=='l' or ch=='L' or ch=='m' or ch=='M' or ch=='n' or ch=='N' or ch=='p' or ch=='P' or ch=='q' or ch=='Q' or ch=='r' or ch=='R' or ch=='s' or ch=='S' or ch=='t' or ch=='T' or ch=='v' or ch=='V' or ch=='w' or ch=='W' or ch=='x' or ch=='X' or ch=='y' or ch=='Y' or ch=='z' or ch=='Z'):
+		    print("Consonant")
+else:
+    print("invalid")
