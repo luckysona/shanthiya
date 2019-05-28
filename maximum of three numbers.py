698d17b3bf567619f9a1551ffffaec518e6fcfe5
luckysona <50876072+luckysona@users.noreply.github.com>
2019-05-27 22:33:37 -0700

    Update maximum of three numbers.py

--- maximum of three numbers.py
+++ maximum of three numbers.py
@@ -1,9 +1,14 @@
-num1=4
-num2=10
-num3=12
-if(num1>num2 and num1>num3):
-  print(num1)
-elif(num2>num3 and num2>num1):
-  print(num2)
+a=int(input())
+n=input().split()
+num1=n[0]
+num2=n[1]
+num3=n[2]
+if(num1>num2):
+  if(num1>num3):
+    print(num1)
+  else:
+    print(num3)
+elif(num2>num3):
+    print(num2)
 else:
   print(num3)
